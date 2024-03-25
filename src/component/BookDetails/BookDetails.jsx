369f648e9139ee233b899c219d36f784de2ea94d
atikxcode <liadbd20@gmail.com>
2024-03-26 03:52:28 +0600

    showing the book details after clicked on a certain book

--- src/component/BookDetails/BookDetails.jsx
+++ src/component/BookDetails/BookDetails.jsx
@@ -1,6 +1,5 @@
-
 import { useEffect, useState } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 
 const BookDetails = () => {
   const books = useLoaderData();
@@ -9,15 +8,12 @@ const BookDetails = () => {
 
   useEffect(() => {
     const idInt = parseInt(id)
-    console.log(idInt)
-    console.log(id)
-   console.log(books)
-  
+    
 
    
     const bookData = books.find(book => book.Id === idInt);
     setBook(bookData)
-    console.log(book)
+    
        
   }, [books, id, book])
   
@@ -25,8 +21,44 @@ const BookDetails = () => {
 
   
   return (
-    <div>
+    <div className="container mx-auto my-10">
+
+      <div className="flex gap-10">
+
+      <div className="bg-[#F3F3F3] p-20 flex justify-center rounded-lg mb-6">
+      <img className="items-center " src={book?.image} alt="" />
+      </div>
+
+      <div>
+      <h2 className="text-[#131313] text-[40px] font-bold mb-4">{book?.bookName}</h2>
+      <h2 className="text-[#131313CC] text-[20px] font-medium mb-6">By: {book?.author}</h2>
+      <hr />
+      <h2 className="mt-4 mb-4 text-[#131313CC] text-[20px] font-medium">{book?.category}</h2>
+      <hr />
       
+      <p className="mt-6 text-[#131313B3] text-[16px] leading-[26px] mb-6"><span className="text-[#131313] text-[16px] font-bold">Review:</span> {book?.review}</p>
+
+      <p className="flex gap-3 items-center mb-6">
+        <span className="text-[#131313] text-[16px] font-bold">Tag</span>
+        <span className="text-[#23BE0A] text-[16px] font-medium bg-[#F3F3F3] p-3 rounded-[30px]">{book?.tags[0]}</span>
+        <span className="text-[#23BE0A] text-[16px] font-medium bg-[#F3F3F3] p-3 rounded-[30px]">{book?.tags[1]}</span>
+      </p>
+      <hr />
+
+      <p className="text-[#131313B3] text-[16px] mt-6">Number of Pages: <span className="text-[#131313] text-[16px] font-semibold ml-4"> {book?.totalPages}</span></p>
+      <p className="text-[#131313B3] text-[16px]">Publisher: <span className="text-[#131313] text-[16px] font-semibold ml-4"> {book?.publisher}</span></p>
+      <p className="text-[#131313B3] text-[16px]">Year of publishing: <span className="text-[#131313] text-[16px] font-semibold ml-4"> {book?.yearOfPublishing}</span></p>
+      <p className="text-[#131313B3] text-[16px] mb-8">Rating: <span className="text-[#131313] text-[16px] font-semibold ml-4"> {book?.rating}</span></p>
+
+      <div className="flex gap-4">
+        <Link><button className="border-[1px] border-[#1313134D] text-[#131313] text-[18px] font-bold pt-[12px] pb-[12px] pr-[30px] pl-[30px] rounded-[8px]">Read</button></Link>
+        <Link><button className=" text-white text-[18px] font-bold pt-[12px] pb-[12px] pr-[30px] pl-[30px] rounded-[8px] bg-[#50B1C9]">Wishlist</button></Link>
+        
+      </div>
+      </div>
+
+      </div>
+
     </div>
   );
 };
