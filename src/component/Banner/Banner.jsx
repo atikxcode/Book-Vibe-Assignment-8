3422878752097b5038e08cc01ffdebb2858c88f6
atikxcode <liadbd20@gmail.com>
2024-03-27 08:15:14 +0600

    finished the responsive part

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -6,17 +6,17 @@ const Banner = () => {
     <div className="container mx-auto my-8 bg-[#1313130D] p-14 rounded-[24px] h-[554px]">
 
       
-      <div className="flex justify-around ">
+      <div className="flex flex-col lg:flex-row justify-around ">
 
-        <div className="flex flex-col justify-center gap-8">
-          <h2 className="text-[#131313] w-[526px] font-playFair text-[56px] font-bold leading-[84px]">Books to freshen up your bookshelf</h2>
+        <div className="flex flex-col justify-center gap-2 lg:gap-8">
+          <h2 className="text-[#131313] w-[90%] lg:w-[526px] font-playFair text-[30px] lg:text-[56px] font-bold leading-[38px] lg:leading-[84px]">Books to freshen up your bookshelf</h2>
           <div>
-          <Link to={'/listedbooks'}><button className="bg-[#23BE0A] pt-[15px] pb-[15px] pr-[20px] pl-[20px] text-[20px] rounded-lg text-white font-bold font-workSans">View The List</button></Link>
+          <Link to={'/listedbooks'}><button className="bg-[#23BE0A] py-[10px] lg:py-[15px] px-[10px] lg:px-[20px] text-[18px] lg:text-[20px] rounded-lg text-white font-bold font-workSans">View The List</button></Link>
           </div>
         </div>
 
         <div>
-          <img src="/public/assets/pngwing 1.png" alt="" />
+          <img className="w-[240px] lg:w-auto" src="/public/assets/pngwing 1.png" alt="" />
         </div>
 
         </div>
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,45 +1,45 @@
 import { NavLink } from "react-router-dom";
-import './Header.css'
+import './Header.css';
 import { useState } from "react";
 
 const Header = () => {
-
- 
-
-
-
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-   <div className="container mx-auto my-10 font-workSans">
+    <div className="container mx-auto my-14 font-workSans">
+      <div className="flex justify-between items-center">
+        
+        <div>
+          <h2 className="text-[#131313] text-[20px] lg:text-[28px] font-bold">Book Vibe</h2>
+        </div>
 
-    <div className="flex justify-between">
-      
-    <div>
-      <h2 className="text-[#131313] text-[28px] font-bold">Book Vibe</h2>
-    </div>
+        
+        <div className="block lg:hidden">
+          <button onClick={() => setMenuOpen(!menuOpen)} className="text-[#131313] focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          </button>
+        </div>
 
-   
-     
-        <div className="flex gap-12 ">
-        <a><NavLink to='/'>Home</NavLink></a>
-        <a><NavLink to='/listedbooks'>Listed Books</NavLink></a>
-        <a><NavLink to='/pages'>Pages to Read</NavLink></a>
-        <a><NavLink to='/topbooks'>Top Rated Books</NavLink></a>
-        <a><NavLink to='/blog'>Blogs</NavLink></a>
+       
+        <nav className={`lg:flex lg:items-center lg:w-auto ${menuOpen ? '' : 'hidden'}`}>
+          <div className="flex flex-col lg:flex-row gap-4 lg:gap-12">
+            <NavLink to='/' className="nav-link" onClick={() => setMenuOpen(false)}>Home</NavLink>
+            <NavLink to='/listedbooks' className="nav-link" onClick={() => setMenuOpen(false)}>Listed Books</NavLink>
+            <NavLink to='/pages' className="nav-link" onClick={() => setMenuOpen(false)}>Pages to Read</NavLink>
+            <NavLink to='/topbooks' className="nav-link" onClick={() => setMenuOpen(false)}>Top Rated Books</NavLink>
+            <NavLink to='/blog' className="nav-link" onClick={() => setMenuOpen(false)}>Blogs</NavLink>
+          </div>
+        </nav>
+
+       
+        <div className="flex flex-col lg:flex-row gap-4 font-medium">
+          <button className="bg-[#23BE0A] text-white py-2 px-4 rounded-lg">Sign In</button>
+          <button className="bg-[#59C6D2] text-white py-2 px-4 rounded-lg">Sign Up</button>
+        </div>
       </div>
-      
-   
-    
-
-   
-
-    <div className="flex gap-4">
-      <button className="bg-[#23BE0A] text-white pt-[15px] pb-[15px] pr-[25px] pl-[25px] rounded-lg">Sign In</button>
-      <button className="bg-[#59C6D2] text-white pt-[15px] pb-[15px] pr-[25px] pl-[25px] rounded-lg">Sign Up</button>
     </div>
-
-    </div>
-   </div>
   );
 };
 
